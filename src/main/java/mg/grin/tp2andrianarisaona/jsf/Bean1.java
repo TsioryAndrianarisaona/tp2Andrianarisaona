e30fb47b12cffffe7852c65566c87c47b7d4dea9
Tsiory Andrianarisaona <tsiory.andrianarisaona@hotmail.com>
2022-04-06 08:40:30 +0300

    tp3 Done

--- src/main/java/mg/grin/tp2andrianarisaona/jsf/Bean1.java
+++ src/main/java/mg/grin/tp2andrianarisaona/jsf/Bean1.java
@@ -38,4 +38,7 @@ private int nombre;
     public String afficher() {
         return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
     }
+      public String afficherFormulaire__4() {
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
 }
